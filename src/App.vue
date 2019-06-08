3c1d2f28b83033b5443a44bfb0f2eada3aa7f23f
VladaMamutova <vlada.mamutova@gmail.com>
2019-06-09 01:07:25 +0300

    feat: add base logic
    Added main structure of webpage.
    Tested different tags of vue.
    Added list of pizza sizes and ingridients with its name, price and unique id. Used v-for and v-key (for id) to display data as a list onto a webpage.
    Updated some dependencies.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,66 @@
 <template>
   <div id="app">
     <h1>Pizza-Designer</h1>
+    <p>{{ message }}</p>
+    <p v-if="restaurantIsOpen">{{ title }}</p>
+    <img :src="pizzaImage" :height="width">
+    <p>Размеры пиццы:
+      <span v-for="size in pizzaSizes" :key="size">{{ size }} </span>
+    </p>
+    <p>Ингридиенты: </p>
+    <div>
+      <div v-for="ingridient in pizzaIngridients" :key="ingridient.id">
+        <p>{{ ingridient.name }} - {{ingridient.price }} руб.
+        <button v-on:click="addToPizza(ingridient)">Добавить</button>
+        <button v-on:click="deleteFromPizza(ingridient)">Удалить</button>
+        </p>
+      </div>
+    </div>
+  <p>Общая стоимость: {{ fullPrice }} руб.</p>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App'
+  data () {
+    return {
+      message: 'Hello Vue.js!',
+      title: 'Craft your own pizza!',
+      pizzaImage: 'https://wallbox.ru/wallpapers/main/201632/82aa5d73f46ceda.jpg',
+      width: 200,
+      restaurantIsOpen: true,
+      pizzaSizes: [35, 40],
+      fullPrice: 0,
+      pizzaIngridients: [
+        {
+          id: 1,
+          name: 'Бекон',
+          price: 50
+        },
+        {
+          id: 2,
+          name: 'Салями',
+          price: 40
+        },
+        {
+          id: 3,
+          name: 'Помидоры',
+          price: 30
+        }
+      ]
+    }
+  },
+  methods: {
+    addToPizza: function (ingridient) {
+      this.fullPrice += ingridient.price
+    },
+    deleteFromPizza (ingridient) {
+      this.fullPrice -= ingridient.price
+    }
+  }
 }
+
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -9,5 +9,39 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  data: {
+    message: 'Hello Vue.js!',
+    title: 'Craft your own pizza!',
+    pizzaImage: 'https://wallbox.ru/wallpapers/main/201632/82aa5d73f46ceda.jpg',
+    width: 200,
+    restaurantIsOpen: true,
+    pizzaSizes: [35, 40],
+    fullPrice: 0,
+    pizzaIngridients: [
+      {
+        id: 1,
+        name: 'Бекон',
+        price: 50
+      },
+      {
+        id: 2,
+        name: 'Салями',
+        price: 40
+      },
+      {
+        id: 3,
+        name: 'Помидоры',
+        price: 30
+      }
+    ]
+  },
+  methods: {
+    addToPizza: function (ingridient) {
+      this.fullPrice += ingridient.price
+    },
+    deleteFromPizza (ingridient) {
+      this.fullPrice -= ingridient.price
+    }
+  }
 })
